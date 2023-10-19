332f7086bc2e90152dd2511131bf373532cbf221
Vitor Soller <21.01444-2@maua.br>
2023-10-19 09:31:34 -0300

    remove unecessary txt

--- src/main/java/group/mpntm/server/Server.java
+++ src/main/java/group/mpntm/server/Server.java
@@ -5,10 +5,6 @@ import group.mpntm.client.ClientSocket;
 import java.io.IOException;
 import java.net.ServerSocket;
 
-// Com esse novo servidor, cada cliente consegue se conectar  ao servidor, uma nova thread é criada e o servidor recebe informação de ambos os clientes
-// Existe um número máximo por conta da porta, 16^4 = 65536 clientes!
-
-
 public class Server {
     public static final String ADDRESS = "127.0.0.1"; // IP Address local do servidor
     public static final int PORT = 4000; // ou 3334
