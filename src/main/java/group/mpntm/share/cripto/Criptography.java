4b81c7a3bc4dc199170223e693d0204b8ca93136
João Vitor <21.01075-7@maua.br>
2023-10-21 15:43:03 -0300

    testing criptography methods

--- src/main/java/group/mpntm/share/cripto/Criptography.java
+++ src/main/java/group/mpntm/share/cripto/Criptography.java
@@ -23,17 +23,32 @@ import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 
 public class Criptography {
+    private static String publicKeyPath = "src\\main\\java\\group\\mpntm\\share\\cripto\\chave.publica";
+    private static String privateKeyPath = "src\\main\\java\\group\\mpntm\\share\\cripto\\chave.privada";
+
     public static void main(String[] args) {
         try{
-            String path = "src\\main\\java\\group\\mpntm\\share\\cripto\\";
-            generatePubPrivKeys(new File (path+"chave.publica"), new File (path+"chave.privada"));
+            generatePubPrivKeys();
         }
         catch (Exception e){
             e.printStackTrace();
         }
+
+        String mensagem = "Andre eh muito engracado 123";
+        try {
+            String msgCript = encryptRSA(mensagem);
+            String msgDecrypt = decryptRSA(msgCript);
+
+            System.out.println("Mensagem original: " + mensagem);
+            System.out.println("Mensagem criptografada: " + msgCript);
+            System.out.println("Mensagem descriptografada: " + msgDecrypt);
+        } catch (Exception e) {
+
+        }
+        
     }
 
-    public static void generatePubPrivKeys(File fPub, File fPvk) throws   IOException, NoSuchAlgorithmException, CertificateException, KeyStoreException, InvalidAlgorithmParameterException {  
+    public static void generatePubPrivKeys() throws   IOException, NoSuchAlgorithmException, CertificateException, KeyStoreException, InvalidAlgorithmParameterException {  
         final int RSAKEYSIZE = 1024;
         KeyPairGenerator kpg = KeyPairGenerator.getInstance ("RSA");  
         kpg.initialize (new RSAKeyGenParameterSpec(RSAKEYSIZE, RSAKeyGenParameterSpec.F4));  
@@ -42,33 +57,39 @@ public class Criptography {
         PublicKey    oPub  = kpr.getPublic();  
 
         //-- Gravando a chave publica em formato serializado  
+        File fPub = new File (publicKeyPath);
         ObjectOutputStream oos = new ObjectOutputStream (new FileOutputStream (fPub));  
         oos.writeObject (oPub);  
         oos.close();  
 
         //-- Gravando a chave privada em formato serializado  
+        File fPvk = new File (privateKeyPath);
         oos = new ObjectOutputStream (new FileOutputStream (fPvk));  
         oos.writeObject (oPriv);  
         oos.close();  
     }
 
-    public static String encryptRSA (byte[] txt, File fPub) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException, IOException, ClassNotFoundException {
+    public static String encryptRSA (String txt) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException, IOException, ClassNotFoundException {
+        byte[] byteTxt = txt.getBytes("ISO-8859-1");
+        File fPub = new File (publicKeyPath);
         ObjectInputStream ois = new ObjectInputStream (new FileInputStream (fPub));  
         PublicKey iPub = (PublicKey) ois.readObject();  
         ois.close();  
         Cipher rsacf = Cipher.getInstance ("RSA");  
         rsacf.init (Cipher.ENCRYPT_MODE, iPub); 
-        byte[] binaryCipher = rsacf.doFinal(txt);
+        byte[] binaryCipher = rsacf.doFinal(byteTxt);
         return new String (binaryCipher, "ISO-8859-1");
     }
     
-   public static String decryptRSA (byte[] txt, File fPrv) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException, IOException, ClassNotFoundException {  
+   public static String decryptRSA (String txt) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException, IOException, ClassNotFoundException {  
+        byte[] byteTxt = txt.getBytes("ISO-8859-1");
+        File fPrv = new File (privateKeyPath);
         ObjectInputStream ois = new ObjectInputStream (new FileInputStream (fPrv));  
         PrivateKey iPrv = (PrivateKey) ois.readObject();  
         ois.close();  
         Cipher rsacf = Cipher.getInstance ("RSA");  
         rsacf.init (Cipher.DECRYPT_MODE, iPrv); 
-        byte[] binaryText = rsacf.doFinal (txt); 
+        byte[] binaryText = rsacf.doFinal (byteTxt); 
         return new String (binaryText, "ISO-8859-1");
    }
 }
