3a92880af0f4b9f9c069bda2c778840f26d9ab9e
João Vitor <21.01075-7@maua.br>
2023-10-22 10:39:59 -0300

    adding cript pass for admin login into db and create method to retrieve it

--- src/main/java/group/mpntm/server/database/repo/RepositoryMySQL.java
+++ src/main/java/group/mpntm/server/database/repo/RepositoryMySQL.java
@@ -1,6 +1,7 @@
 package group.mpntm.server.database.repo;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import group.mpntm.server.database.connector.ConnectorFactory;
@@ -10,7 +11,23 @@ public class RepositoryMySQL {
     private static String login_table_name = "LOGIN";
 
     public static void main(String[] args) {
-        createLogin("admin", "admin");
+        // Test create login for admin
+        // createLogin("admin", "admin");
+
+        // Test get pass for admin
+        String pass = getPass("admin");
+        System.out.println("Senha do admin criptografada: " + pass);
+        String passDecrypt;
+        try {
+            passDecrypt = Criptography.decryptRSA(pass);
+        } catch (Exception e) {
+            passDecrypt = "Deu merda";
+        }
+        System.out.println("Senha do admin descriptografada: " + passDecrypt);
+
+        // Test get pass for non existent user
+        String pass2 = getPass("nonexistent");
+        System.out.println("Senha do nonexistent criptografada: " + pass2);
     }
 
     public static void createLogin(String name, String password) {
@@ -42,6 +59,29 @@ public class RepositoryMySQL {
             ConnectorFactory.closeConn(conn, stmt);
         }
 
+    }
+
+    public static String getPass(String name){
+        String sqlSelect = "SELECT * FROM " + login_table_name + " WHERE name = ?";
+        Connection conn = ConnectorFactory.getConn();
+        PreparedStatement stmt = null;
+        ResultSet rs;
+        String pass = "-1";
+        try{   
+            stmt = conn.prepareStatement(sqlSelect);
+            stmt.setString(1, name);
+            rs = stmt.executeQuery();
+            while(rs.next()){
+                pass = rs.getString("pass");
+            }
+        }
+        catch(SQLException ex){   
+            System.out.println("Erro ao consultar os dados" + ex.toString());
+        }
+        finally{
+            ConnectorFactory.closeConn(conn, stmt);
+        }       
+        return pass;
     }
 
 }
