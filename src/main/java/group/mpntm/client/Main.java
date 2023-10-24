ca47ecdcc6ec2205d420bb13b9dbb112ea28c0db
Vitor Soller <vgsoller@gmail.com>
2023-10-24 12:48:31 -0300

    implement bundle in right folder

--- src/main/java/group/mpntm/client/Main.java
+++ src/main/java/group/mpntm/client/Main.java
@@ -1,11 +1,14 @@
 package group.mpntm.client;
 
+import java.util.ResourceBundle;
+
 public class Main {
 
     public static void main(String[] args) {
 
         Client client = new Client();
         ClientScreen clientScreen = new ClientScreen(client);
+
 
     }
 }
