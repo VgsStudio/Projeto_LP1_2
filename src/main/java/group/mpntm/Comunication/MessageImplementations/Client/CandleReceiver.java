91e338430ec3c8d68f65f14fdcff44e7683003ef
Vitor Soller <vgsoller@gmail.com>
2023-10-25 23:02:21 -0300

    add candle receiver

---
+++ src/main/java/group/mpntm/Comunication/MessageImplementations/Client/CandleReceiver.java
@@ -0,0 +1,17 @@
+package group.mpntm.Comunication.MessageImplementations.Client;
+
+import com.google.gson.Gson;
+import group.mpntm.Comunication.MessageImplementations.IClientMessageImplementation;
+import group.mpntm.client.Candle;
+
+public class CandleReceiver implements IClientMessageImplementation {
+
+    @Override
+    public void Activate(String messageContent) {
+        Candle candle = new Gson().fromJson(messageContent, Candle.class);
+
+        System.out.println(candle);
+
+    }
+
+}
