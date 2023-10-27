b5a1d355b04fc6923f96f72df406931d09aa98ae
Vitor Soller <vgsoller@gmail.com>
2023-10-27 15:35:54 -0300

    add error if user cannot connect

--- src/main/java/group/mpntm/client/Main.java
+++ src/main/java/group/mpntm/client/Main.java
@@ -9,21 +9,26 @@ import group.mpntm.Comunication.MesasgeContent.HistoryRequestContent;
 import group.mpntm.Comunication.MessageHandlers.ClientMessageHandler;
 import group.mpntm.Comunication.MessageImplementations.Server.HistoryRequestReceiver;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) throws IOException{
-        
-        ClientCommunication clientCommunication = new ClientCommunication();
+
+        ClientCommunication clientCommunication;
+        try{
+            clientCommunication = new ClientCommunication();
+
 
         ButtonClickingConnection buttonClickingConnection = new ButtonClickingConnection(clientCommunication);
         ClientMessageHandler clientMessageHandler = new ClientMessageHandler(clientCommunication);
-        HistoryButtonPressedEvent.getInstance().AddListener(
-            () -> {
-                HistoryRequestContent historyRequestContent = new HistoryRequestContent(100);
-                var json = new Gson().toJson(historyRequestContent);
-                clientCommunication.SendMessage(json, HistoryRequestReceiver.class.getSimpleName());
-            }
-        );
+//        HistoryButtonPressedEvent.getInstance().AddListener(
+//            () -> {
+//                HistoryRequestContent historyRequestContent = new HistoryRequestContent(100);
+//                var json = new Gson().toJson(historyRequestContent);
+//                clientCommunication.SendMessage(json, HistoryRequestReceiver.class.getSimpleName());
+//            }
+//        );
 
 
         ClientScreen clientScreen = new ClientScreen();
@@ -33,6 +38,12 @@ public class Main {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Não foi possível conectar ao servidor", "Erro", JOptionPane.ERROR_MESSAGE);
+            System.exit(0);
+        }
+
 
     }
 }
