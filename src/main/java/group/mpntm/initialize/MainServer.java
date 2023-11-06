ec8b45319cb3d8490cfcf88352ef30c73a8f8ae5
João Vitor <21.01075-7@maua.br>
2023-11-06 20:53:59 -0300

    creating main for client and server

--- src/main/java/group/mpntm/initialize/MainServer.java
+++ src/main/java/group/mpntm/initialize/MainServer.java
@@ -1,10 +1,10 @@
-package group.mpntm.server.comunication;
+package group.mpntm.initialize;
 
 import java.io.IOException;
 
 import group.mpntm.server.comunication.domain.ServerCommunication;
 
-public class Main {
+public class MainServer {
     public static void main(String[] args) throws IOException {
         ServerCommunication.execute();
     }
